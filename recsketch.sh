3184bfec11d2a0eb6dad871f7fb3fa53893259c6
edyother <ed@edyother.com>
2013-06-22 15:55:29 -0400

    update

--- recsketch.sh
+++ recsketch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # I use this to help with getting down ideas when I'm writing music.
-# ./rsketch.sh [ songname ]
+# ./recsketch.sh [ songname ]
 
 # set variables
 TIME=$(date +%Y-%m-%d_%T)
@@ -33,14 +33,14 @@ read C
 if [ $C = k ]
   then
   sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
-  ./rsketch.sh $1
+  ./recsketch.sh $1
 elif [ $C = x ]
   then
   sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
 elif [ $C = d ]
   then 
   rm $1/$TIME.ogg
-  ./rsketch.sh $1
+  ./recsketch.sh $1
 fi
 rm $1/$TIME.ogg
 clear
