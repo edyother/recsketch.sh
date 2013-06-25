3ddccaafe8e162061dd240b5abb7084381498ea3
edyother <ed@edyother.com>
2013-06-25 19:08:45 -0400

    update

--- recsketch.sh
+++ recsketch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # I use this to help with getting down ideas when I'm writing music.
 # ./recsketch.sh [ songname ]
 
@@ -17,7 +18,7 @@ figlet "$1"
 echo " ----------------"
 echo "| CTRL+C to stop |"
 echo " ----------------"
-rec $1/$TIME.ogg
+rec $1/new.ogg
 
 # Options After Recoring
 echo "--------------------------"
@@ -30,18 +31,17 @@ echo "--------------------------"
 echo "Discard & Exit       ENTER"
 echo "--------------------------"
 read C
-if [ $C = k ]
+if [ $C = k ] 2> /dev/null
   then
-  sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
+  sox $1/new.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1 2> /dev/null
   ./recsketch.sh $1
-elif [ $C = x ]
+elif [ $C = x ] 2> /dev/null
   then
-  sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
-elif [ $C = d ]
+  sox $1/new.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1 2> /dev/null
+elif [ $C = d ] 2> /dev/null
   then 
-  rm $1/$TIME.ogg
+  rm $1/new.ogg
   ./recsketch.sh $1
 fi
-rm $1/$TIME.ogg
-clear
-ls -1 $1/*.ogg
+rm ./$1/new.ogg 2> /dev/null
+
